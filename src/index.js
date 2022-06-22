058e05ac1ee6b05f14023779c1db20f07b18b8c3
simon waronja <simon.waronja@student.moringaschool.com>
2022-06-22 16:53:58 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector("form").addEventListener('submit', (e) => {
+    e.preventDefault();
+    tasks(e.target.value)
+    document.querySelector('form').reset();
+  })
 });
+
+function tasks(task) {
+  const input = document.getElementById("new-task-description");
+  let li = document.createElement("li");
+  let btn = document.createElement("button")
+  btn.addEventListener('click', removeTask)
+  btn.textContent = 'X';
+  li.inner
+  li.innerHTML = `${input.value} `;
+  li.appendChild(btn)
+  document.querySelector("#tasks").appendChild(li);
+}
+
+function removeTask(e) {
+  e.target.parentNode.remove();
+}
